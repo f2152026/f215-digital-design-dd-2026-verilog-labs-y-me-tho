627ba9c1a9567e517fd4bc3c3f750a2ffd77a664
Rishab Jha <f20240969@pilani.bits-pilani.ac.in>
2026-09-19 09:13:34 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,11 +1,16 @@
 // tb.v
 // Starter testbench template -- YOU complete this file.
 
-module tb;
+module tb #(parameter WIDTH=8,parameter DEPTH=8);
 
   // TODO: declare the inputs and outputs
-
+    reg [$clog2(DEPTH)-1:0] t_sel;
+    wire [WIDTH-1:0] t_dout;
   // TODO: instantiate DUT here
+    lut #(.WIDTH(WIDTH),.DEPTH(DEPTH)) DUT(
+      .sel(t_sel),
+      .dout(t_dout)
+    );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -16,12 +21,24 @@ module tb;
     end
   end
 
+  integer i;
   initial begin
     // TODO: apply different input combinations
+    for(i=0;i<DEPTH;i=i+1) begin
+      t_sel=i;
+      #1;
+      if(t_dout!==i*i)
+        $display("ERROR: sel=%0d, expected=%0d, got=%0d",i,i*i,t_dout);
+      else  
+        $display("PASS: sel=%0d, dout=%0d",i,t_dout);
+    end 
+    $finish;
 
   end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+  initial begin
+    $monitor($time, " SEL=%b, DOUT=%b",t_sel,t_dout); // change as required
+  end
 
 endmodule
+
